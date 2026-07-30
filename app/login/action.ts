228d191962016362ceb6a9aace8c2959b41eacc2
alim0129 <alim0129@student.monash.edu>
2026-07-30 21:53:50 +1000

    feat: apply user validation on login

--- app/login/action.ts
+++ app/login/action.ts
@@ -3,13 +3,24 @@
 import z from "zod";
 import { createSession, deleteSession } from "lib/session";
 import { redirect } from "next/navigation";
+import {verify} from "argon2"
+import pool from "@/lib/db"
+import { RowDataPacket } from "mysql2";
 
 const loginSchema = z.object({
     email: z.string().email({message: "Invalid email address"}),
     password: z.string().min(6, {message: "Password must be at least 6 characters long"})
 })
 
+interface UserRow extends RowDataPacket{
+    id: number,
+    email: string;
+    password: string
+}
+
 export async function login(prevState: any, formData: FormData) {
+
+    //Input validation
     const result = loginSchema.safeParse(Object.fromEntries(formData.entries()));
 
     //If validation fails, return the errors to the form
@@ -22,17 +33,33 @@ export async function login(prevState: any, formData: FormData) {
     //If validation succeeds, check the credentials correctness
     const {email, password} = result.data;
 
-    //If credentials are incorrect, return an error to the form
-    if(email !== ""){
+    //Searhc for user
+    const sql = "SELECT id, email, password FROM core_user WHERE email = ? LIMIT 1";
+    const [rows] = await pool.execute<UserRow[]>(
+        sql,
+        [email]
+    );
+
+    const user = rows[0];
+
+    //If user is not found, then email is invalid
+    if(!user){
         return {
-            errors:{
-                password: ["Invalid email or password"]
-            }
+            errors : {password: ["Invalid email or password"]}
         }
     }
 
-    await createSession(email);
-    redirect("/");
+    //Check credentials valid or not
+    const isValid = await verify(user.password, password);
+    if(!isValid){
+        return {
+            errors : {password: ["Invalid email or password"]}
+        }
+    } else {
+        await createSession(String(user.id));
+        redirect("/");
+    }
+
 }
 
 export async function logout() {
