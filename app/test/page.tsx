b92d119cdc3a5067ede75c77df60340416b9ccf0
alim0129 <alim0129@student.monash.edu>
2026-08-02 02:45:29 +1000

    feat: add input validation to add product form

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -4,6 +4,8 @@ import Sidebar from "@/components/Sidebar";
 import { useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@base-ui/react";
+import { FormEvent } from "react";
+import z from "zod";
 
 type ProductForm = {
     name: string;
@@ -17,16 +19,146 @@ const emptyForm: ProductForm = {
     barcode: ""
 };
 
+interface Produk {
+    id: number;
+    name: string;
+    price: string;
+    barcode: string;
+};
+
+type ProductFormErrors = Partial<Record<keyof ProductForm, string>>;
+
+const addProductSchema = z.object({
+    name: z.string().min(1, { message: "Name cannot be empty" }),
+    price: z.string().min(1, { message: "Price cannot be empty" }),
+    barcode: z.string().min(1, { message: "Barcode cannot be empty" })
+})
+
+async function getProducts(
+    page: number,
+    pageSize: number,
+    search: string) {
+    try {
+        const params = new URLSearchParams({
+            page: page.toString(),
+            pageSize: pageSize.toString(),
+        });
+        if (search.trim()) {
+            params.append("search", search);
+        }
+        const res = await fetch(`/api/product?${params}`);
+        const data = await res.json();
+        return data;
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+function getPageNumbers(currentPage: number, totalPages: number) {
+    const pages: (number | "...")[] = [];
+    if (totalPages <= 7) {
+        for (let i = 1; i <= totalPages; i++) {
+            pages.push(i);
+        }
+        return pages;
+    }
+    pages.push(1);
+    if (currentPage > 4) {
+        pages.push("...");
+    }
+    const start = Math.max(2, currentPage - 1);
+    const end = Math.min(totalPages - 1, currentPage + 1);
+    for (let i = start; i <= end; i++) {
+        pages.push(i);
+    }
+    if (currentPage < totalPages - 3) {
+        pages.push("...");
+    }
+    pages.push(totalPages);
+    return pages;
+}
 
 
 export default function Test() {
     const pathName = usePathname();
     const [showModal, setShowModal] = useState(false);
+    const [products, setProducts] = useState<Produk[]>([]);
+
+    //Table variables
+    const [page, setPage] = useState(1);
+    const [pageSize] = useState(10);
+    const [total, setTotal] = useState(0);
+    const [totalPages, setTotalPages] = useState(0);
+    const pageNumbers = getPageNumbers(page, totalPages);
+    const [search, setSearch] = useState("");
 
     //Form
-    const [form, setForm] = useState<ProductForm>(emptyForm)
+    const [form, setForm] = useState<ProductForm>(emptyForm);
+    const [formErrors, setErrors] = useState<ProductFormErrors>({});
+
     function updateForm(field: keyof ProductForm, value: string) {
         setForm((current) => ({ ...current, [field]: value }));
+    }
+
+
+    //Submit Form Function
+    async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        const validation = addProductSchema.safeParse(form);
+
+        if (!validation.success) {
+            const errors = validation.error.flatten().fieldErrors;
+            setErrors({
+                name: errors.name?.[0],
+                price: errors.price?.[0],
+                barcode: errors.barcode?.[0],
+            });
+
+            return;
+        }
+
+        setErrors({});
+
+        try {
+            const res = await fetch("/api/product", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    name: form.name,
+                    barcode: form.barcode,
+                    price: Number(form.price),
+                }),
+            });
+
+            if (!res.ok) {
+                throw new Error("Failed to save product");
+            }
+
+            const result = await res.json();
+
+            console.log("Product created:", result);
+
+            // Reload table
+            const data = await getProducts(
+                page,
+                pageSize,
+                search);
+            if (data) {
+                setProducts(data.data);
+                setTotal(data.total);
+                setTotalPages(data.totalPages);
+            }
+
+            // Clear form
+            setForm(emptyForm);
+            setShowModal(false);
+
+        } catch (err) {
+            console.error(err);
+            alert("Failed to save product");
+        }
     }
 
     return (
@@ -77,7 +209,10 @@ export default function Test() {
                         </div>
                         {/* Form */}
                         {/* Form Wrapper */}
-                        <div className="p-[22px]">
+                        <form
+                            className="p-[22px]"
+                            onSubmit={handleSubmit}
+                        >
                             {/* Form Grid */}
                             <div className="grid grid-cols-auto sm:grid-cols-2 gap-4">
                                 {/* Input */}
@@ -90,8 +225,14 @@ export default function Test() {
                                         value={form.name}
                                         onChange={(e) => updateForm("name", e.target.value)}
                                         placeholder="Enter product name"
-                                        className="h-12"
+                                        className={`h-12 ${
+                                            formErrors.name
+                                            ? "border-red-500 focus-visible:border-red-500 focus-visible:ring-red-500/20"
+                                            : ""
+                                        }`}
                                     />
+                                    {/* Error text */}
+                                    {formErrors.name && (<p className="text-xs text-red-500">{formErrors.name}</p>)}
                                 </div>
                                 {/* Input */}
                                 <div className="flex flex-col gap-2 text-[#555] text-sm ">
@@ -103,8 +244,14 @@ export default function Test() {
                                         value={form.price}
                                         onChange={(e) => updateForm("price", e.target.value)}
                                         placeholder="Enter price"
-                                        className="h-12"
+                                        className={`h-12 ${
+                                            formErrors.price
+                                            ? "border-red-500 focus-visible:border-red-500 focus-visible:ring-red-500/20"
+                                            : ""
+                                        }`}
                                     />
+                                    {/* Error text */}
+                                    {formErrors.price && (<p className="text-xs text-red-500">{formErrors.price}</p>)}
                                 </div>
                                 {/* Input */}
                                 <div className="flex flex-col gap-2 text-[#555] text-sm">
@@ -116,24 +263,32 @@ export default function Test() {
                                         value={form.barcode}
                                         onChange={(e) => updateForm("barcode", e.target.value)}
                                         placeholder="Enter barcode"
-                                        className="h-12"
+                                        className={`h-12 ${
+                                            formErrors.barcode
+                                            ? "border-red-500 focus-visible:border-red-500 focus-visible:ring-red-500/20"
+                                            : ""
+                                        }`}
                                     />
+                                    {/* Error text */}
+                                    {formErrors.barcode && (<p className="text-xs text-red-500">{formErrors.barcode}</p>)}
                                 </div>
                             </div>
                             {/* Buttons */}
                             <div className="flex flex-row mt-6 justify-end gap-2">
                                 <Button
                                     className="bg-white text-[#17191c] border border[#dfe2e5] rounded-lg cursor-pointer font-extrabold py-[11px] px-[16px]"
+                                    onClick={() => setShowModal(false)}
                                 >
                                     Cancel
                                 </Button>
                                 <Button
+                                    type="submit"
                                     className="bg-[#17191c] text-white rounded-lg cursor-pointer font-extrabold py-[11px] px-[16px]"
                                 >
                                     Add Product
                                 </Button>
                             </div>
-                        </div >
+                        </form >
                     </div>
                 </div>
             }
