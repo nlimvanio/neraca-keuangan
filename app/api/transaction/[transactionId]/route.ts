9f54ee4c0df14e629057deb796740484ac0e9e57
alim0129 <alim0129@student.monash.edu>
2026-08-16 01:02:09 +1000

    feat: create DELETE method for transactions

---
+++ app/api/transaction/[transactionId]/route.ts
@@ -0,0 +1,87 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { RowDataPacket } from 'mysql2';
+
+import pool from '@/lib/db';
+
+export async function DELETE(
+    request: NextRequest,
+    {params}: { params: Promise<{ transactionId: string }> }
+) {
+    const {transactionId} = await params;
+    if(!transactionId){
+        return NextResponse.json(
+            { message: "Transaction ID not provided" },
+            { status: 400 }
+        );
+    }
+
+    const connection = await pool.getConnection();
+
+    try{
+        await connection.beginTransaction();
+
+
+        const selectProductSql = `
+        SELECT t.quantity, t.id_product 
+        FROM transactions t
+        LEFT OUTER JOIN core_product cp ON cp.id = t.id_product
+        WHERE t.id = ?
+        FOR UPDATE
+        `;
+        
+        const [rows] = await connection.query<RowDataPacket[]>({
+            sql: selectProductSql,
+            values: [transactionId]
+        });
+
+        if(rows.length === 0){
+            await connection.rollback();
+            connection.release();
+            return NextResponse.json(
+                {message: "Invalid transaction ID"},
+                {status: 404}    
+            )
+        };
+
+        const {quantity, id_product} = rows[0]
+
+        const updateProductSql = `
+        UPDATE core_product cp
+        SET stock = stock - ?
+        WHERE cp.id = ?
+        `
+
+        await connection.query({
+            sql: updateProductSql,
+            values: [quantity, id_product]
+        });
+
+        const deleteSql = `
+        DELETE 
+        FROM transactions t 
+        WHERE id = ?
+        `;
+
+        await connection.query({
+            sql : deleteSql,
+            values: [transactionId]
+        });
+
+        await connection.commit();
+        return NextResponse.json(
+            {message: "Transaction deleted succesfuly"},
+            {status: 200}
+        )
+
+    } catch (error){
+        await connection.rollback();
+        console.error(error);
+
+        return NextResponse.json(
+        { message: "Internal Server Error" },
+        { status: 500 }
+        );
+    } finally {
+        connection.release();
+    }
+}
